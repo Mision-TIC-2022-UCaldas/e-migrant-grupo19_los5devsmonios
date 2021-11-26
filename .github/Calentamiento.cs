ea2aa899862de9515d66c4ba904c0ef89e48cf2a
LEON-PC\LEON <vxleonardo@gmail.com>
2021-11-26 10:48:13 -0500

    Prueba 2

--- .github/Calentamiento.cs
+++ .github/Calentamiento.cs
@@ -1,4 +1,16 @@
-namespace Calentamiento
+namespace Calentamiento 
 {
-    
-}
+
+     public class IndMasCorporal
+     {
+        public void calcularImc(Double peso, Double altura)
+         {
+             Double imc =  peso *  Pow(altura, 2);
+             Console.writeline(imc);
+fjhgvfnfghndfncvbn
+
+         }vcbcvbcvbcvb
+
+         calcularimc(2.0, 1.0);
+     }  
+}cvb
